f4cea650bcd1673507ef0e9b939c915fcc2e5fc7
Jayvolt <70279845+Jayvolt@users.noreply.github.com>
2020-10-09 15:36:52 -0400

    First commit

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScript : MonoBehaviour
+{
+    private Rigidbody2D rd2d;
+    public float speed;
+
+    public Text winText;
+    public Text score;
+    private int scoreValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rd2d = GetComponent<Rigidbody2D>();
+        scoreValue = 0;
+        winText.text = "";
+        SetScoreText();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float hozMovement = Input.GetAxis("Horizontal");
+        float vertMovement = Input.GetAxis("Vertical");
+
+        rd2d.AddForce(new Vector2(hozMovement * speed, vertMovement * speed));
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Coin")
+        {
+            scoreValue += 1;
+            SetScoreText();
+            Destroy(collision.collider.gameObject);
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                rd2d.AddForce(new Vector2(0, 3), ForceMode2D.Impulse);
+            }
+        }
+    }
+    void SetScoreText()
+    {
+        score.text = scoreValue.ToString();
+        if (scoreValue == 3)
+        {
+            winText.text = "You Win!";
+        }
+    }
+}
+ 
